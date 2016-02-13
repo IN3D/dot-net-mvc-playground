e4a141e6056b9347adf9228108fc350925e877ee
Eric Hopkins <eric.on.tech@gmail.com>
2016-02-13 18:48:15 -0500

    Remove unused imports

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -1,9 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
-using WebAPIApplication.Models;
 
 namespace WebAPIApplication.Controllers
 {   
